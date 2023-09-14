31238a162ecc889490b974ff9dc8b80bc9983805
washington-shoji <shoji.washington@gmail.com>
2023-09-14 18:52:33 +1000

    -updated main

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
-	server := NewAPIServer(":3000")
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Printf("%+v\n", store)
+
+	if err := store.init(); err != nil {
+		log.Fatal(err)
+	}
+
+	server := NewAPIServer(":3000", store)
 	server.Run()
 }
